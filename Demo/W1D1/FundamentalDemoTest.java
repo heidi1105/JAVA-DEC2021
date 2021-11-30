e1e5d35de6225122057e392ed93b62388c170ba6
heidi1105 <heidi1105@gmail.com>
2021-11-29 17:01:18 -0800

    W1D1 PM Lecture

---
+++ Demo/W1D1/FundamentalDemoTest.java
@@ -0,0 +1,18 @@
+public class FundamentalDemoTest{
+
+    public static void main(String[] args){
+        FundamentalDemo demoApp = new FundamentalDemo();
+        demoApp.printGreeting();
+
+        // System.out.println(demoApp.greeting());
+        // System.out.println(demoApp.greeting("Pepper"));
+        // System.out.println(demoApp.greeting("10"));
+        // System.out.println(demoApp.greeting("Heidi", "Aloha"));
+        // //Your first assignment
+        // demoApp.testing(123);
+        // demoApp.testing("String");
+
+        // demoApp.fixedArr();        
+        demoApp.dynamicArray();
+    }
+}
