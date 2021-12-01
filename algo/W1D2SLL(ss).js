baa9d5a0cc470ee42ed49ce493264425d28fbc93
heidi1105 <heidi1105@gmail.com>
2021-11-30 19:05:54 -0800

    Updated algos

--- algo/W1D2SLL(ss).js
+++ algo/W1D2SLL(ss).js
@@ -5,33 +5,32 @@ class Node {
     }
 }
 
-var a = new Node(1)
-var b = new Node(2)
-var c = new Node(3)
-a.next = b;
-b.next = c; 
 
 
 class SLL {
     constructor() {
             this.head = null;
         }
-    
-    //Determines if the list is empty ***    
+
+    // ------------------------------------- Week 1 Day 2 algo -------------------------------------------
+ 
     isEmpty(){
+        //Determines if the list is empty ***   
     }
 
-    //Create a new node with the given value and inserts it at the back of the list
+
     insertAtBack(val){
-        // instantiate the node with the val
-        // put the new node into the list
-        // 1  -->  2  -->  3 --> null 
-        // 1  -->  2  -->  3 --> 4    --> null
-    }
-    arrToList(arr){
-        //BONUS: Add all the items of an array to the back of the list
+        // Create a new node with the given value and inserts it at the back of the list
+        // HINT: How to find the last node of a Singly Linked List?
+
     }
 
+    // bonus questions 
+    arrToList(arr){
+        //Add all the items of an array to the back of the list
+    }
+
+    //given
     printList(){
         if(!this.head){
             console.log("Empty list");
@@ -42,15 +41,33 @@ class SLL {
             console.log(runner.val);
             runner = runner.next;            
         }
-        // 1  -->  2  -->  3 --> null 
-        //1 --> 2 --> 3--> 4 --> null
     }
+    // ------------------------------------- END OF Week 1 Day 2 algo -------------------------------------------
 
 }
 
+// Constructing nodes connecting each other with .next
+var a = new Node(1)
+var b = new Node(2)
+var c = new Node(3)
+a.next = b;
+b.next = c; 
+
+// Constructing a Singly Linked List with the above nodes 
 var list1 = new SLL();
 list1.head = a;
 list1.printList();
-// 1 , 2, 3 , 100, 200, 300
-// list1.arrToList([100, 200 , 300])
+// list1: (1) --> (2) --> (3) --> NULL
+//       HEAD
+
+
+
+// Test case for arrToList: 
+// list1.arrToList([8, 9 , 10])
 // list1.printList();
+// expected outcome: (1) --> (2) --> (3) --> (8) --> (9) --> (10) --> null
+
+
+
+
+
---
+++ algo/W1D3SLL(ss).js
@@ -0,0 +1,83 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+
+class SLL {
+    constructor() {
+            this.head = null;
+        }
+
+    // ------------------------------------- Week 1 Day 3 algo -------------------------------------------
+    
+    insertAtFront(val){
+        // construct a node with the val
+        // make the new node as the front (HINT: What is the front of a Singly Linked List)
+    }
+
+
+    removeHead(){
+        // Remove the head (HINT: What will be the new head?)
+
+    }
+
+    //*** bonus question ***
+    average(){
+        // Find the average of all the nodes inside a Singly Linked List
+        // HINT: How to get an average? What information do we need to generate an average?
+        // BONUS: What if one of the nodes stored String instead of int? How do you want to handle that edge case?
+    }
+    // ------------------------------------- END OF Week 1 Day 3 algo -------------------------------------------
+
+
+
+    // If you want to use the following methods, please fill in the logic on your own. (You can copy from Discord if you want to)
+    // ------------------------------------- Week 1 Day 2 algo -------------------------------------------
+    isEmpty(){} //empty method holder
+
+    insertAtBack(val){} //empty method holder
+
+    arrToList(arr){} //empty method holder
+
+    printList(){
+        if(!this.head){
+            console.log("Empty list");
+            return
+        }
+        var runner = this.head;
+        while(runner){ // while(runner != null)
+            console.log(runner.val);
+            runner = runner.next;            
+        }
+    }
+
+    // ------------------------------------- END OF Week 1 Day 2 algo -------------------------------------------
+
+
+}
+
+var a = new Node(1)
+var b = new Node(2)
+var c = new Node(3)
+a.next = b;
+b.next = c; 
+var list1 = new SLL();
+list1.head = a;
+list1.printList();
+// list1: (1) --> (2) --> (3) --> null
+
+
+
+
+
+
+
+
+
+
+
+
+
